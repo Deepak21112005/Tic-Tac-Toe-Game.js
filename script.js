2fe1d317cd0fdb01840f800c046e3bb3996a7427
Deepak21112005 <deepakkumarpanigrahi555@gmail.com>
2025-10-01 20:44:07 +0530

    +1 try in JS

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-const innerBoxes = document.querySelectorAll(".innerBox");
-const position = innerBoxes.getBoundingClientRect();
-innerBoxes.forEach(box => {
-    let height = position.height;
-    let width = position.width;
-    box.addEventListener("click", function() {
-        height = 180px;
-        width = 180px;
+const Circle = document.createElement('div');
+Circle.id = 'circle';
+const Cross = document.createElement('div');
+Cross.id = 'cross';
+
+const Forward = document.createElement('div');
+Forward.id = 'forward';
+const Backward = document.createElement('div');
+Backward.id = 'backward';
+Cross.append(Forward);
+Cross.prepend(Backward);
+
+const innerBoxes = document.querySelectorAll(".innerBox")
+//console.log(innerBoxes);
+let count = 0;
+    innerBoxes.foreach((Box) => {
+        Box.addEventListener("click",() => {
+            if (Box.firstElementChild){
+                alert("--You can't Overwrite--");
+                return;
+            }
+            if (count % 2 === 0){
+                Box.appendChild(Cross.cloneNode(true));
+                console.log("Cross Placed");
+            }
+            else {
+                Box.appendChild(Circle.cloneNode(true));
+                console.log("Circle Placed");  
+            }
+            count++;
+        });
     });
-});
